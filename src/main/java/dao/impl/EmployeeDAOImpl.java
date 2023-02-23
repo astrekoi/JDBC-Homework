dc218bf8c9a89730bd9bce6da96e16977f77200f
Andrey Zinchenko <astrokoit@gmail.com>
2023-02-23 21:48:31 +0700

    Переделано дз

--- src/main/java/dao/impl/EmployeeDAOImpl.java
+++ src/main/java/dao/impl/EmployeeDAOImpl.java
@@ -1,57 +1,95 @@
 package dao.impl;
 
 import dao.EmployeeDAO;
+import jbdc.ConnectionManager;
+import model.City;
 import model.Employee;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import util.HibernateUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class EmployeeDAOImpl implements EmployeeDAO {
 
+    private static final String FIND_ALL = "SELECT * FROM employee INNER JOIN city ON employee.city_id=city.city_id";
+    private static final String INSERT = "INSERT INTO employee (id, first_name, last_name, gender, age, city_id) VALUES (?, ?, ?, ?, ?, ?)";
+    private static final String FIND_BY_ID = "SELECT * FROM employee INNER JOIN city ON employee.city_id=city.city_id AND employee.id=?";
+    private static final String UPDATE = "UPDATE employee SET first_name=? WHERE id=?";
+    private static final String DELETE = "DELETE FROM employee WHERE id=?";
+
     @Override
-    public void createEmployee(Employee employee) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.saveOrUpdate(employee);
-            transaction.commit();
+    public void createEmployee(Employee employee) throws SQLException {
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(INSERT)) {
+            statement.setLong(1, employee.getId());
+            statement.setString(2, employee.getFirstName());
+            statement.setString(3, employee.getLastName());
+            statement.setString(4, employee.getGender());
+            statement.setInt(5, employee.getAge());
+            statement.setLong(6, employee.getCityId().getId());
+            statement.executeUpdate();
         }
     }
 
     @Override
-    public Employee getEmployeeById(Long id) {
-        return HibernateUtil.getSessionFactory().openSession().get(Employee.class, id);
+    public Employee getEmployeeById(Long id) throws SQLException {
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID)) {
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return createEmployeeFromResultSet(resultSet);
+            } else {
+                return null;
+            }
+        }
     }
 
     @Override
-    public List<Employee> getAllEmployees() {
-        List<Employee> employees;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            employees = new ArrayList<>(session.createQuery("FROM Employee", Employee.class).list());
-            transaction.commit();
+    public List<Employee> getAllEmployees() throws SQLException {
+        List<Employee> employees = new ArrayList<>();
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(FIND_ALL)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                employees.add(createEmployeeFromResultSet(resultSet));
+            }
         }
         return employees;
     }
 
     @Override
-    public void updateEmployee(Employee employee) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.update(employee);
-            transaction.commit();
+    public void updateEmployee(Employee employee) throws SQLException {
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(UPDATE)) {
+            statement.setString(1, employee.getFirstName());
+            statement.setLong(2, employee.getId());
+            statement.executeUpdate();
         }
     }
 
     @Override
-    public void deleteEmployeeById(Long id) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            Employee employee = session.get(Employee.class, id);
-            session.delete(employee);
-            transaction.commit();
+    public void deleteEmployeeById(Long id) throws SQLException {
+        try (Connection connection = ConnectionManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(DELETE)) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
         }
+    }
+
+    private Employee createEmployeeFromResultSet(ResultSet resultSet) throws SQLException {
+        Long id = resultSet.getLong("id");
+        String firstName = resultSet.getString("first_name");
+        String lastName = resultSet.getString("last_name");
+        String gender = resultSet.getString("gender");
+        Integer age = resultSet.getInt("age");
+        Long cityId = resultSet.getLong("city_id");
+        String cityName = resultSet.getString("city_name");
+        City city = new City(cityId, cityName);
+        return new Employee(id, firstName, lastName, gender, age, city);
     }
 }
--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,46 +1,24 @@
 package model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Entity
 @Getter
 @Setter
-@Table(name = "employee")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+
     private Long id;
 
-    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "gender", nullable = false)
     private String gender;
 
-    @Column(name = "age", nullable = false)
     private Integer age;
 
-    @Column(name = "city_id", nullable = false)
-    private Integer cityId;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "city_id", referencedColumnName = "city_id", insertable = false, updatable = false)
-    private City city;
-
-    public Employee() {}
-
-    public Employee(String firstName, String lastName, String gender, Integer age, Integer cityId) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.age = age;
-        this.cityId = cityId;
-    }
+    private City cityId;
 }
